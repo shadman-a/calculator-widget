3aac2ecd426e88abd0ece41b1e98fe026054df66
shadman-a <ahmedshadman966@gmail.com>
2023-08-09 00:27:29 -0400

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Shadman Ahmed on 8/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
